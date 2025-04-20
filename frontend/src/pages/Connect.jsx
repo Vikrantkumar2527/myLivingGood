b3bc5e7b84857e92d8eb84af1c387a9e7179c516
VikrantKumar2527 <mahurvikky04@gmail.com>
2025-04-20 20:01:34 +0530

    adding responsiveness to connect page

--- frontend/src/pages/Connect.jsx
+++ frontend/src/pages/Connect.jsx
@@ -8,8 +8,8 @@ function Connect() {
             <div className="flex justify-center">
                 <Title1 text1={'CONTACT'} text2={'US'}/>
             </div>
-            <div className="flex justify-center mt-8">
-                <img className="w-80 h-80 mr-12" src={assets.contact_img} alt="" />
+            <div className="flex flex-col gap-4 sm:gap-0 sm:flex-row justify-center mt-8">
+                <img className="w-40 sm:w-80 h-40 sm:h-80 mr-12" src={assets.contact_img} alt="" />
                 <div className="flex flex-col justify-center">
                     <p className="mt-2 ">OUR STORE</p>
                     <p className="mt-2 text-gray-400">Beta 1 Block c-177</p>
